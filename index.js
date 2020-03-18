5a3efc30383f711ed7c0986ae5a4a0cfc5473ab8
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-03-18 15:22:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+
+const findMatching = (arr, obj) => arr.filter(unit => unit.toUpperCase() === obj.toUpperCase())
+
+const fuzzyMatch = (arr, obj) => arr.filter(unit => unit.startsWith(obj))
+
+const matchName = (arr, obj) => arr.filter(unit => unit.name === obj)
